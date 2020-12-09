752db3b0f5d0f9c76b804496d12b6c7b96c69d2f
Adrian S MUresan <adriansmuresan@gmail.com>
2020-12-09 10:08:32 -0600

    Declares the constants that we will be working with

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav1');
+const nav2 = document.getElementById('nav2');
+const nav3 = document.getElementById('nav3');
+const nav4 = document.getElementById('nav4');
+const nav5 = document.getElementById('nav5');
